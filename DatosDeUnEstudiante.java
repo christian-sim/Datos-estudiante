d6db21b3ea00657d135f8d03e3ae714e088b995a
bladimir <christian.simbana@ister.edu.ec>
2019-07-09 00:41:52 -0500

    version1

---
+++ DatosDeUnEstudiante.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class DatosDeUnEstudiante {
+    public static void main(String[] args) {
+String nombre,apellido,direccion,especialidad,email,celular;
+int edad;
+Scanner lee=new Scanner(System.in);
+System.out.println("ingrese su NOMBRE:");
+nombre=lee.next();
+System.out.println("ingrese su APELLIDO:");
+apellido=lee.next();
+System.out.println("ingrese su DIRECCION:");
+direccion=lee.next();
+System.out.println("ingrese su ESPECIALIDAD:");
+especialidad=lee.next();
+System.out.println("ingrese su EMAIL:");
+email=lee.next();
+System.out.println("ingrese su NUMERO CELULAR:");
+celular=lee.next();
+System.out.println("ingrese su EDAD:");
+edad=lee.nextInt();
+
+Datos obj1=new Datos(nombre,apellido,direccion,especialidad,email,celular,edad);
+obj1.imprimirDatos();
+}   
+}
---
+++ datos.java
@@ -0,0 +1,75 @@
+public class Datos {   
+    String nombre;
+    String apellido;
+    String direccion;
+    String especialidad;
+    String email;
+    String celular;
+    int edad;
+public Datos(String nombre,String apellido,String direccion,String especialidad,String email,String celular,int edad ){
+this.nombre=nombre;
+this.apellido=apellido;
+this.direccion=direccion;
+this.especialidad=especialidad;
+this.email=email;
+this.celular=celular;
+this.edad=edad;}
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    public String getEspecialidad() {
+        return especialidad;
+    }
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getCelular() {
+        return celular;
+    }
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+public void imprimirDatos(){
+System.out.println("su Nombre es:");
+System.out.println(getNombre());
+System.out.println("su Apellido es:");
+System.out.println(getApellido());
+System.out.println("su Direccion es:");
+System.out.println(getDireccion());
+System.out.println("su Especialidad es:");
+System.out.println(getEspecialidad());
+System.out.println("su Email es:");
+System.out.println(getEmail());
+System.out.println("su Celular es:");
+System.out.println(getCelular());
+System.out.println("su Edad es:");
+System.out.println(getEdad());
+}
+}
